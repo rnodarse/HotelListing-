0974d76f6f845e6c0068970f2b928230c06fb894
raulrgn85 <raulrgn85@gmail.com>
2023-04-03 20:47:57 -0400

    Added method GenerateToken() to AuthManager to be able to generate the token

--- HotelListing.API/Services/AuthManager.cs
+++ HotelListing.API/Services/AuthManager.cs
@@ -3,6 +3,10 @@ using HotelListing.API.Contracts;
 using HotelListing.API.Data;
 using HotelListing.API.Models.Users;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace HotelListing.API.Services
 {
@@ -10,11 +14,13 @@ namespace HotelListing.API.Services
     {
         private readonly IMapper _mapper;
         private readonly UserManager<ApiUser> _userManager;
+        private readonly IConfiguration _configuration;
 
-        public AuthManager(IMapper mapper, UserManager<ApiUser> userManager)
+        public AuthManager(IMapper mapper, UserManager<ApiUser> userManager, IConfiguration configuration)
         {
             this._mapper = mapper;
             this._userManager = userManager;
+            this._configuration = configuration;
         }
         public async Task<bool> Login(LoginDto loginDto)
         {
@@ -44,6 +50,34 @@ namespace HotelListing.API.Services
             }
             // If the creation of the user was successfull, it will return no errors
             return result.Errors;
+        }
+        private async Task<string> GenerateToken(ApiUser user)
+        {
+            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"]));
+
+            var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var roleClaims = roles.Select(x => new Claim(ClaimTypes.Role, x)).ToList();
+
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            }
+            .Union(userClaims).Union(roleClaims);
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JwtSettings:Issuer"],
+                audience: _configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToInt32(_configuration["JwtSettings:DurationInMinutes"])),
+               signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
